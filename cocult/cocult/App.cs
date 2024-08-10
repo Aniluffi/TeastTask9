444bf7d0610146985321c536052067b588392664
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2024-08-10 20:50:51 +0300

    Add files via upload

---
+++ cocult/cocult/App.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cocult
+{
+    /// <summary>
+    /// класс для работы с приложением
+    /// </summary>
+    class App
+    {
+        /// <summary>
+        /// список для хранения данных о введенных фигурах 
+        /// </summary>
+        public static ListFigure<Figure> listEnteredShapes = new ListFigure<Figure>();
+
+        /// <summary>
+        /// лист для хранения команд
+        /// </summary>
+        private List<IComand> _comands = new List<IComand>(); 
+
+        /// <summary>
+        /// конструктор, который добавляет команды
+        /// </summary>
+        public App()
+        {
+            Reg();
+        }
+
+        /// <summary>
+        /// метод который запускает программу
+        /// </summary>
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n1 - квадрат, вводим сторону одну" +
+                    "\n2 - прямоугольник, вводим два стороны" +
+                    "\n3 - Круг, вводим радиус" +
+                    "\n4 - треугольник, вводим три стороны" +
+                    "\n5 - многоугольник" +
+                    "\n6 - вывести сумму всех периметров" +
+                    "\n7 - вывести сумму всех площадей" +
+                    "\n8 - вывод списка всех фигур ранее введенных" +
+                    "\n9 - вывод суммы площадей всех квадратов" +
+                    "\n10 - вывод суммы площадей всех кругов" +
+                    "\n11 - вывод суммы площадей всех прямоугольников" +
+                    "\n12 - вывод суммы площадей всех треугольников" +
+                    "\n13 - вывод суммы площадей всех многоугольников" +
+                    "\n14 - вывод суммы периметров всех квадратов" +
+                    "\n15 - вывод суммы периметров всех кругов" +
+                    "\n16 - вывод суммы периметров всех прямоугольников" +
+                    "\n17 - вывод суммы периметров всех треугольников" +
+                    "\n18 - вывод суммы периметров всех многоугольников" +
+                    "\n19 - выйти из программы\n");
+
+                Console.WriteLine("Введите номер действия:");
+                string comand = Console.ReadLine();
+                string[] words;
+
+                if (comand.Split().Length == 2)
+                { 
+                    words = comand.Split(" ", 2);
+                    SearhComand(words[0], words[1]);
+                } 
+                else
+                {
+                    words = comand.Split(" ", 1);
+                    SearhComand(words[0], "");
+                }
+            }
+        }
+        /// <summary>
+        /// метод для поиска команды
+        /// </summary>
+        /// <param name="comand">команда</param>
+        private void SearhComand(string comand,string parametrs)
+        {
+            foreach(IComand el in _comands)
+            {
+                if (el.NameComand == comand) el.Execute(parametrs);
+                
+            }
+            Console.Clear();
+            Console.WriteLine("Ошибка в записи команды");
+        }
+
+        public static List<int> ToParametrs(string parametr)
+        {
+            string[] a = parametr.Split();
+            List<int> parametrs = new List<int>();
+
+            foreach(var c in a)
+            {
+                
+                if (int.TryParse(c,out int d))
+                {
+                    parametrs.Add(Convert.ToInt32(c));
+                }
+            }
+            
+            return parametrs;
+        }
+
+        /// <summary>
+        /// метод для регистрации команд
+        /// </summary>
+        private void Reg()
+        {
+            _comands.Add(new ComandSquare());
+            _comands.Add(new ComandRectangle());
+            _comands.Add(new ComandTringle());
+            _comands.Add(new ComandCircle());
+            _comands.Add(new ComandPolygon());
+            _comands.Add(new ComandAllP());
+            _comands.Add(new ComandAllS());
+            _comands.Add(new ComandPrintEnteredShape());
+            _comands.Add(new ComandPrintSumSSquare());
+            _comands.Add(new ComandPrintSumSCircle());
+            _comands.Add(new ComandPrintSumSRectangle());
+            _comands.Add(new ComandPrintSumSTriangle());
+            _comands.Add(new ComandPrintSumSPolygon());
+            _comands.Add(new ComandPrintSumPSquare());
+            _comands.Add(new ComandPrintSumPCircle());
+            _comands.Add(new ComandPrintSumPRectangle());
+            _comands.Add(new ComandPrintSumPTriangle());
+            _comands.Add(new ComandPrintSumPPolygon());
+            _comands.Add(new ComandExit());
+        }
+
+
+    }
+}
---
+++ cocult/cocult/Comands.cs
@@ -0,0 +1,531 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cocult
+{
+    /// <summary>
+    /// команда для нахождения периметра или площади квадрата
+    /// </summary>
+    class ComandSquare : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandSquare()
+        {
+            NameComand = "квадрат";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Square sq = new Square(App.ToParametrs(data));
+
+            App.listEnteredShapes.Add(sq);
+
+            Console.WriteLine("Вы желаете найти S(1) или P(2)");
+
+            int comand = Convert.ToInt32(Console.ReadLine());
+
+            if (comand == 1) Console.WriteLine($"S = {sq.S()}");
+            else if (comand == 2) Console.WriteLine($"P = {sq.P()}");
+        }
+    }
+    /// <summary>
+    /// команда для нахождения периметра или площади прямоугольника
+    /// </summary>
+    class ComandRectangle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandRectangle()
+        {
+            NameComand = "прямоугольник";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Rectangle sq = new Rectangle(App.ToParametrs(data));
+
+            App.listEnteredShapes.Add(sq);
+
+            Console.WriteLine("Вы желаете найти S(1) или P(2)");
+
+            int comand = Convert.ToInt32(Console.ReadLine());
+
+            if (comand == 1) Console.WriteLine($"S = {sq.S()}");
+            else if (comand == 2) Console.WriteLine($"P = {sq.P()}");
+        }
+    }
+    /// <summary>
+    /// команда для нахождения периметра или площади прямоугольника
+    /// </summary>
+    class ComandTringle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandTringle()
+        {
+            NameComand = "треугольник";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Triangle sq = new Triangle(App.ToParametrs(data));
+
+            App.listEnteredShapes.Add(sq);
+
+            Console.WriteLine("Вы желаете найти S(1) или P(2)");
+
+            int comand = Convert.ToInt32(Console.ReadLine());
+
+            if (comand == 1) Console.WriteLine($"S = {sq.S()}");
+            else if (comand == 2) Console.WriteLine($"P = {sq.P()}");
+        }
+    }
+    /// <summary>
+    /// команда для нахождения периметра или площади прямоугольника
+    /// </summary>
+    class ComandCircle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+         public string NameComand { get; set; }
+
+        public ComandCircle()
+        {
+            NameComand = "круг";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute( string data)
+        {
+            Console.Clear();
+            Circle sq = new Circle(App.ToParametrs(data));
+
+            App.listEnteredShapes.Add(sq);
+
+            Console.WriteLine("Вы желаете найти S(1) или P(2)");
+
+            int comand = Convert.ToInt32(Console.ReadLine());
+
+            if (comand == 1) Console.WriteLine($"S = {sq.S()}");
+            else if (comand == 2) Console.WriteLine($"P = {sq.P()}");
+        }
+    }
+    /// <summary>
+    /// команда для нахождения периметра или площади прямоугольника
+    /// </summary>
+    class ComandPolygon : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPolygon()
+        {
+            NameComand = "многоугольник";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Polygon sq = new Polygon(App.ToParametrs(data));
+
+            App.listEnteredShapes.Add(sq);
+
+            Console.WriteLine("Вы желаете найти S(1) или P(2)");
+
+            int comand = Convert.ToInt32(Console.ReadLine());
+
+            if (comand == 1) Console.WriteLine($"S = {sq.S()}");
+            else if (comand == 2) Console.WriteLine($"P = {sq.P()}");
+        }
+    }
+    /// <summary>
+    /// команда для нахождения суммы периметров всех введенных фигур
+    /// </summary>
+    class ComandAllP : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandAllP()
+        {
+            NameComand = "сумма_периметров_всех_введенных_фигур";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумма всех P = {App.listEnteredShapes.PType<Figure>()}");
+        }
+    }
+    /// <summary>
+    /// команда для нахождения суммы площади всех введенных фигур
+    /// </summary>
+    class ComandAllS : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandAllS()
+        {
+            NameComand = "сумма_площадей_всех_введенных_фигур";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумма всех S = {App.listEnteredShapes.SType<Figure>()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода всех введенных фигур
+    /// </summary>
+    class ComandPrintEnteredShape : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintEnteredShape()
+        {
+            NameComand = "вывод_всех_введенных_фигур";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Введенные фигуры:\n  {App.listEnteredShapes.ToString()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода суммы площадей всех введенных квадратов квадратов
+    /// </summary>
+    class ComandPrintSumSSquare : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintSumSSquare()
+        {
+            NameComand = "вывод_суммы_площадей_всех_введенных_квадратов";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумма всех S квадратов = {App.listEnteredShapes.SType<Square>()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода суммы площадей всех введенных кругов
+    /// </summary>
+    class ComandPrintSumSCircle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintSumSCircle()
+        {
+            NameComand = "вывод_суммы_площадей_всех_введенных_кругов";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумма всех S кругов = {App.listEnteredShapes.SType<Circle>()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода суммы площадей всех введенных прямоугольников
+    /// </summary>
+    class ComandPrintSumSRectangle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintSumSRectangle()
+        {
+            NameComand = "вывод_суммы_площадей_всех_введенных_прямоугольников";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумма всех S прямоугольников = {App.listEnteredShapes.SType<Rectangle>()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода суммы площадей всех введенных треугольников
+    /// </summary>
+    class ComandPrintSumSTriangle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintSumSTriangle()
+        {
+            NameComand = "вывод_суммы_площадей_всех_введенных_треугольников";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумма всех S треугольников = {App.listEnteredShapes.SType<Triangle>()}");
+        }
+    }
+
+    /// <summary>
+    /// команда для вывода суммы площадей всех введенных многоугольников
+    /// </summary>
+    class ComandPrintSumSPolygon : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+    public ComandPrintSumSPolygon()
+    {
+        NameComand = "вывод_суммы_площадей_всех_введенных_многоугольников";
+    }
+
+    /// <summary>
+    /// метод выполнения команды
+    /// </summary>
+    /// <param name="data">параметры задания</param>
+    public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумарная площадь многоугольников - {App.listEnteredShapes.SType<Polygon>()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода суммы периметров всех введенных квадратов
+    /// </summary>
+    class ComandPrintSumPSquare : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+    public ComandPrintSumPSquare()
+    {
+        NameComand = "вывод_суммы_периметров_всех_введенных_квадратов";
+    }
+
+    /// <summary>
+    /// метод выполнения команды
+    /// </summary>
+    /// <param name="data">параметры задания</param>
+    public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумарная периметров квадратов - {App.listEnteredShapes.PType<Square>()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода суммы периметров всех введенных кругов
+    /// </summary>
+    class ComandPrintSumPCircle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintSumPCircle()
+        {
+            NameComand = "вывод_суммы_периметров_всех_введенных_кругов";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумарная периметров кругов - {App.listEnteredShapes.PType<Circle>()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода суммы периметров всех введенных прямоугольников
+    /// </summary>
+    class ComandPrintSumPRectangle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintSumPRectangle()
+        {
+            NameComand = "вывод_суммы_периметров_всех_введенных_прямоугольников";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумарная периметров прямоугольников - {App.listEnteredShapes.PType<Rectangle>()}");
+        }
+    }
+    /// <summary>
+    /// команда для вывода суммы периметров всех введенных треугольников
+    /// </summary>
+    class ComandPrintSumPTriangle : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintSumPTriangle()
+        {
+            NameComand = "вывод_суммы_периметров_всех_введенных_треугольников";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумарная периметров треугольников - {App.listEnteredShapes.PType<Triangle>()}");
+        }
+    }
+
+    /// <summary>
+    /// команда для вывода суммы периметров всех введенных многоугольников
+    /// </summary>
+    class ComandPrintSumPPolygon : IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandPrintSumPPolygon()
+        {
+            NameComand = "вывод_суммы_периметров_всех_введенных_многоугольников";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Console.WriteLine($"Сумарная периметров многоугольников - {App.listEnteredShapes.PType<Polygon>()}");
+        }
+    }
+    /// <summary>
+    /// команда для выхода из приложения
+    /// </summary>
+    class ComandExit: IComand
+    {
+        /// <summary>
+        /// имя команды
+        /// </summary>
+        public string NameComand { get; set; }
+
+        public ComandExit()
+        {
+            NameComand = "выход";
+        }
+
+        /// <summary>
+        /// метод выполнения команды
+        /// </summary>
+        /// <param name="data">параметры задания</param>
+        public void Execute(string data)
+        {
+            Console.Clear();
+            Environment.Exit(0);
+            Console.WriteLine($"Программа завершена!");
+        }
+    }
+}
